907656ee1f06c5dd9a92a4ec7c28888490380339
jaswanthsaireddy <pasalajaswanthsair8@gmail.com>
2025-02-02 18:41:02 +0530

    style changes

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,7 +7,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies)
   return (
     <div className='bg-black'>
-      <div className=' relative mt-0 md:-mt-[22rem] z-20 pl-4 md:pl-24'>
+      <div className=' relative mt-0 md:-mt-[20rem] z-20 pl-4 md:pl-24'>
         <MovieList title={"Now Playing"} movies = {movies.nowPlayingMovies}/>
         <MovieList title={"Upcoming"} movies = {movies.upcomingMovies}/>
         <MovieList title={"Top Rated"} movies = {movies.topRatedMovies}/>
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className= "  w-screen aspect-video absolute pt-[10%] md:pt-[16%] px-4 md:px-24 text-white bg-gradient-to-tr from-black">
+    <div className= "  w-screen aspect-video absolute pt-[10%] md:pt-[12%] px-4 md:px-24 text-white bg-gradient-to-tr from-black mg-10">
       <h1 className=' text-lg md:text-4xl font-bold'>{title}</h1>
-      <p className='hidden md:block py-6 w-1/3'>{overview}</p>
+      <p className='hidden md:block py-3 w-[40%]'>{overview}</p>
       <div className='flex my-2 md:my-0'>
         <button className=' bg-white text-black font-bold py-1 md:py-2 px-4 md:px-14 text-lg md:text-2xl rounded-lg hover:opacity-70'>Play</button>
         <button className=' bg-gray-700 hidden md:block font-bold py-2 px-9 text-2xl ml-3 rounded-lg  text-white opacity-70 hover:opacity-90'>More Info</button>
